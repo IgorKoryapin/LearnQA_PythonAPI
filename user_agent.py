f8790795eb6d21691c78572a1d8c793e4e9f27cb
IgorKoryapin <koryapinig@gmail.com>
2022-10-11 16:57:08 +0500

    ext 13 commit

---
+++ user_agent.py
@@ -0,0 +1,25 @@
+import requests
+import pytest
+
+@pytest.mark.parametrize(
+    "test_input, expected",
+    [
+        ('Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
+         {'platform': 'Mobile', 'browser': 'No', 'device': 'Android'}),
+        ('Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1',
+         {'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'}),
+        ('Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)', {'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'}),
+        ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0', {'platform': 'Web', 'browser': 'Chrome', 'device': 'No'}),
+        ('Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1', {'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'})
+    ]
+)
+def test_eval(test_input, expected):
+
+    response = requests.get("https://playground.learnqa.ru/ajax/api/user_agent_check", headers={"User-Agent": test_input})
+    response_json = response.json()
+    response_json.pop('user_agent')
+    assert response.json() == expected, f"vuyuuuiuobi"
+
+
+
+
